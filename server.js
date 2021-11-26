bd1d9af75bac8de2039385a2b31bc76a266bf53e
miguelkristian17 <Miguelkmartinez@gmail.com>
2021-11-25 20:48:39 -0800

    server fix

--- server.js
+++ server.js
@@ -79,6 +79,4 @@ app.get("/index", (req, res) => {
 
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(process.env.PORT || 5000, () => console.log(`Server is running on port ${PORT}`));
